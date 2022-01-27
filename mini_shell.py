17fcd7dfa4f38bc7329c2bbead205f61ae1e27a4
minhtrannhat <minhtrannhat2001@gmail.com>
2022-01-27 11:58:57 -0500

    Feat: added some comments in mini_shell.py and configured black for code formatting

--- mini_shell.py
+++ mini_shell.py
@@ -3,22 +3,31 @@ import getpass
 import socket
 
 
+# the interactive shell will execute all commands and quit on 'exit' command
 def main():
     while True:
         command = input(f"{get_username_from_OS()}@{get_hostname_from_OS()}$ ")
         if command == "exit":
             break
         else:
-            print(command)
+            execute_command(command)
 
 
+# get username to display
 def get_username_from_OS() -> str:
     return getpass.getuser()
 
 
+# get hostname to display
 def get_hostname_from_OS() -> str:
     return socket.gethostname()
 
 
+# execute all commands from user
+def execute_command(command: str):
+    print(command)
+
+
 if "__main__" == __name__:
+    print("Welcome to the mini shell !\n")
     main()
